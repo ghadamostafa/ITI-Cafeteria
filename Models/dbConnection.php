e521b52dfebc655844a74e0bd14fabcf4cf86430
Yassminelbialy <yassminelbialy@gmail.com>
2020-03-15 14:14:06 -0700

    final

--- Models/dbConnection.php
+++ Models/dbConnection.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect("localhost:3308", "root", "", "cafeteria");
+$connect = mysqli_connect("localhost:3307", "root", "", "iti_cafeteria");
 if (!$connect) {
 	echo "cann't connect";
 	echo mysqli_connect_error();
